e83361db3dcce50fe78b5483854427c11326e05e
apiraccini-alk <alessio.piraccini@akemy.com>
2024-11-22 17:39:18 +0100

    Update prompts

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -16,7 +16,7 @@ def llm(system_prompt: str, user_text: str, oai: bool = USE_OPENAI) -> str:
     Args:
         system_prompt (str): The system prompt to guide the model.
         user_text (str): The user input text.
-        oai (bool, optional): Flag to use OpenAI or Groq. Defaults to True.
+        oai (bool, optional): Flag to use OpenAI or Groq.
 
     Returns:
         str: The generated response from the language model.
@@ -48,14 +48,13 @@ def llm(system_prompt: str, user_text: str, oai: bool = USE_OPENAI) -> str:
 
 
 prompts = {
-    "CLEANER": """You will receive a transcript of a meeting and you will need to summarize it. The speaker is a senior team member conducting an onboarding session for a new joiner of the team.
-Your task is to clean the transcript of irrelevant information. It should become a written document, as if it was a documentation that somebody could study on in order to understand the tasks that the new member is going to conduct.
-Therefore, you should both summarize the information but also be complete and thorough in the explanation.
-Don't cite people involved in the meeting. The text should be impersonal.
+    "CLEANER": """You are an expert transcript cleaner. Your will receive a segment of a transcript from a Teams meeting, where a senior team member is conducting an onboarding session for new joiners.
+Your task is to clean the contents of the transcript, keeping only the relevant information and discarding any irrelevant, conversational or nonsensical text.
+It should become a piece of documentation, accessible by someone that didn't follow the recording in order to study for their onboarding.
+You should be verbose and ensure that every bit of relevant information is reported in the cleaned transcript. You should be thorough and write at least a few sentences for each topic and if a particular topic is discussed, you should explain it and give a definition of it.
+Common terms that may be misspelled include: "PowerBI", "SharePoint", "Dataiku", "Amplifon", "CRM", "Customer", "Prospect", "Lead", "Hot", "Cold", "Churn", "EDW", "BDFL" (instead of DFL), "Appointments", "Tests", "Trials", "Sales", "ALK", "Job standard", "Consol customer key", "All events" (instead of Olive). Ensure these and any other misspelled words are corrected in the cleaned transcript.
 Don't ever cite the lecturer or the students.
-Just output the cleaned content in a way that could be accessible by someone that didn't follow the recording.
-You must not translate or alter the language used, the output text should be in Italian.
-Common terms that may be misspelled include: "PowerBI", "SharePoint", "Dataiku", "Amplifon", "CRM", "Customer", "Prospect", "Lead", "Hot", "Cold", "Churn", "EDW", "BDFL" (instead of DFL), "Appointments", "Tests", "Trials", "Sales", "ALK", "Job standard", "Consol customer key", "All events" (instead of Olive). Ensure these and any other misspelled words are corrected in the cleaned transcript.""",
+You must not translate or alter the language used, the output text should be in Italian. This is of vital importance!""",
     "FORMATTER": """You will receive a text which is obtained by joining several trascript chunks processed by an LLM. 
 Your task is to format the text in a coherent way. The final text should be written in markdown, use headers, subheaders and (when useful) bullet points. 
 The main sections and subsections should be meaningful, well organized and not too short. You can reorganize the content as you see fit to achieve this objective.
